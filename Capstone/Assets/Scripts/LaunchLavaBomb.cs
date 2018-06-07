0f8cfeeb5b52df57acd21703f648c37b1dab907e
Albert Lardizabal <albie06@gmail.com>
2018-06-07 01:45:50 -0400

    Environment and Launch Scripts
    1. Added sounds
    2. Added 3D models
    3. Basic lava bomb launcher physics

---
+++ Capstone/Assets/Scripts/LaunchLavaBomb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchLavaBomb : MonoBehaviour
+{
+
+    public GameObject lavaBomb;
+
+    void Start()
+    {
+        LaunchBomb();
+    }
+
+    public void LaunchBomb()
+    {
+        Rigidbody lavaBombRigidbody = Instantiate(lavaBomb.GetComponent<Rigidbody>(), transform.position, transform.rotation) as Rigidbody;
+        ;
+        lavaBombRigidbody.AddForce(transform.forward * 800);
+        GetComponent<AudioSource>().Play();
+    }
+}
